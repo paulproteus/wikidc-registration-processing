396128dbe0ef78160949c746d31436288a33758d
jeremyb <jeremy@tuxmachine.com>
2012-07-10 03:48:50 +0000

    fixes to not rely on column order
    (was either a bug or input has changed over time)

--- crunchbadges.py
+++ crunchbadges.py
@@ -8,11 +8,8 @@
 import csv
 import sys
 
-# Burn off first line; we don't need the old column names
-sys.stdin.readline()
-
 # Read stdin as a CSV file
-csv_in = csv.reader(sys.stdin)
+csv_in = csv.DictReader(sys.stdin)
 
 # This list will collect all column names it encounters.
 column_names = ["Uid",
@@ -26,16 +23,17 @@ column_names = ["Uid",
 dict_rows = []
 for row in csv_in:
     # Create an initial dict for the first six columns
-    dict_row = {"Uid":                row[0],
-                "Order ID":           row[1],
-                "Order product ID":   row[2],
-                "Payer's first name": row[3],
-                "Payer's last name":  row[4],
-                "Status":             row[5]}
+    dict_row = {"Uid":                row["Uid"],
+                "Order ID":           row["Order ID"],
+                "Order product ID":   row["Order product ID"],
+                "Payer's first name": row["Payer's first name"],
+                "Payer's last name":  row["Payer's last name"],
+                "Status":             row["Status"],
+    }
   
     # Split lines of attributes column, filtering out blanks
     attr_lines = filter(lambda s: len(s) != 0,
-                        row[6].splitlines())
+                        row['Ordered product attributes'].splitlines())
 
     # Stick each attribute into its corresponding column
     for line in attr_lines:
@@ -51,21 +49,11 @@ for row in csv_in:
     dict_rows.append(dict_row)
 
 # Write to stdout as a CSV
-csv_out = csv.writer(sys.stdout)
+csv_out = csv.DictWriter(sys.stdout, column_names)
 
-# Print out a row for the column headings
-csv_out.writerow(column_names)
+## Print out a row for the column headings
+csv_out.writeheader()
 
 # Now, print out the rows
 for dict_row in dict_rows:
-    # Order the columns in the order we encountered them
-    columns = []
-    for column_name in column_names:
-        # Output the column, or "" if it doesn't exist on this row
-        if column_name in dict_row:
-            columns.append(dict_row[column_name])
-        else:
-            columns.append("");
-
-    # Write out the row
-    csv_out.writerow(columns)
+    csv_out.writerow(dict_row)
